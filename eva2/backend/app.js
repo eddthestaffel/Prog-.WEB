a53c0e1dc02472cce16832f3f39f179bd84a4254
Edd <mrpotato387@gmail.com>
2026-05-13 11:54:13 -0400

    Modificación
    Se eliminó la columna de cubierta y se modifico la validación de menú para que acepte un >=0, esto significa que calorías no debe ser negativo.

--- eva2/backend/app.js
+++ eva2/backend/app.js
@@ -25,18 +25,18 @@ app.get('/canchas/:id', async (req, res) => {
 })
 
 app.post('/canchas', async (req, res) => {
-    const { nombre, tipo_superficie, capacidad_personas, precio_hora, cubierta, horario_apertura, horario_cierre } = req.body; // Extraemos los datos del cuerpo de la petición
+    const { nombre, tipo_superficie, capacidad_personas, precio_hora, horario_apertura, horario_cierre } = req.body; // Extraemos los datos del cuerpo de la petición
     // Validación básica
-    if (!nombre || !tipo_superficie || !capacidad_personas || !precio_hora || !cubierta == null || !horario_apertura || !horario_cierre) {
+    if (!nombre || !tipo_superficie || !capacidad_personas || !precio_hora || !horario_apertura || !horario_cierre) {
         return res.status(400).json({ error: 'Todos los campos son obligatorios' });
     }
     try {
         // Usamos "?" como placeholders para prevenir inyección SQL
-        const query = 'INSERT INTO canchas_deportivas (nombre, tipo_superficie, capacidad_personas, precio_hora, cubierta, horario_apertura, horario_cierre) VALUES (?, ?, ?, ?, ?, ?, ?)';
-        const [result] = await db.query(query, [nombre, tipo_superficie, capacidad_personas, precio_hora, cubierta, horario_apertura, horario_cierre]);
+        const query = 'INSERT INTO canchas_deportivas (nombre, tipo_superficie, capacidad_personas, precio_hora, horario_apertura, horario_cierre) VALUES (?, ?, ?, ?, ?, ?)';
+        const [result] = await db.query(query, [nombre, tipo_superficie, capacidad_personas, precio_hora, horario_apertura, horario_cierre]);
         res.status(201).json({
             mensaje: 'Usuario guardado con éxito',
-            id: result.insertId // Retornamos el ID generado en la DB
+            id: result.insertId
         });
     } catch (error) {
         res.status(500).json({ error: 'Error al guardar en la base de datos' });
@@ -45,19 +45,19 @@ app.post('/canchas', async (req, res) => {
 
 app.put('/canchas/:id', async (req, res) => {
     var id = req.params.id;
-    const {nombre,tipo_superficie,capacidad_personas,precio_hora,cubierta,horario_apertura,horario_cierre} = req.body; // Extraemos los datos del cuerpo de la petición
+    const { nombre, tipo_superficie, capacidad_personas, precio_hora, horario_apertura, horario_cierre } = req.body; // Extraemos los datos del cuerpo de la petición
     // Validación básica
-    if (!nombre || !tipo_superficie || !capacidad_personas || !precio_hora || !cubierta == null || !horario_apertura || !horario_cierre) {
+    if (!nombre || !tipo_superficie || !capacidad_personas || !precio_hora || !horario_apertura || !horario_cierre) {
         return res.status(400).json({ error: 'Todos los campos son obligatorios' });
     }
     try {
         // Usamos "?" como placeholders para prevenir inyección SQL
-        const query = 'UPDATE canchas_deportivas SET nombre= ?, tipo_superficie=?, capacidad_personas=?, precio_hora=?, cubierta=?, horario_apertura=?, horario_cierre=?  where id = ? ';
+        const query = 'UPDATE canchas_deportivas SET nombre= ?, tipo_superficie=?, capacidad_personas=?, precio_hora=?, horario_apertura=?, horario_cierre=?  where id = ? ';
         console.log(query);
-        const [result] = await db.query(query, [nombre, tipo_superficie, capacidad_personas, precio_hora, cubierta, horario_apertura, horario_cierre, id]);
+        const [result] = await db.query(query, [nombre, tipo_superficie, capacidad_personas, precio_hora, horario_apertura, horario_cierre, id]);
         res.status(201).json({
             mensaje: 'Usuario guardado con éxito',
-            id: result.insertId // Retornamos el ID generado en la DB
+            id: result.insertId
         });
     } catch (error) {
         res.status(500).json({ error: 'Error al guardar en la base de datos' });
@@ -105,11 +105,18 @@ app.get('/menu/:id', async (req, res) => {
 
 })
 
+/*Dentro de post y put de menú se acepta el 0 como mayor o igual, más no tener calorias negativas*/
+
 app.post('/menu', async (req, res) => {
-    const { nombre_plato,descripcion,precio,categoria,calorias,disponible } = req.body; // Extraemos los datos del cuerpo de la petición
+    const { nombre_plato, descripcion, precio, categoria, calorias, disponible } = req.body; // Extraemos los datos del cuerpo de la petición
     // Validación básica
-    if (!nombre_plato || !descripcion || !precio || !categoria || !calorias == null || !disponible == null) {
+    if (!nombre_plato || !descripcion || !precio || !categoria || calorias == null || disponible == null) {
         return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+    }
+    if (calorias < 0) {
+        return res.status(400).json({
+            error: 'Las calorías no pueden ser negativas'
+        });
     }
     try {
         // Usamos "?" como placeholders para prevenir inyección SQL
@@ -117,7 +124,7 @@ app.post('/menu', async (req, res) => {
         const [result] = await db.query(query, [nombre_plato, descripcion, precio, categoria, calorias, disponible]);
         res.status(201).json({
             mensaje: 'Plato agregado con éxito',
-            id: result.insertId // Retornamos el ID generado en la DB
+            id: result.insertId
         });
     } catch (error) {
         res.status(500).json({ error: 'Error al guardar en la base de datos' });
@@ -126,10 +133,15 @@ app.post('/menu', async (req, res) => {
 
 app.put('/menu/:id', async (req, res) => {
     var id = req.params.id;
-    const {nombre_plato,descripcion,precio,categoria,calorias,disponible} = req.body; // Extraemos los datos del cuerpo de la petición
+    const { nombre_plato, descripcion, precio, categoria, calorias, disponible } = req.body; // Extraemos los datos del cuerpo de la petición
     // Validación básica
-    if (!nombre_plato || !descripcion || !precio || !categoria || !calorias == null || !disponible == null) {
+    if (!nombre_plato || !descripcion || !precio || !categoria || calorias == null || disponible == null) {
         return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+    }
+    if (calorias < 0) {
+        return res.status(400).json({
+            error: 'Las calorías no pueden ser negativas'
+        });
     }
     try {
         // Usamos "?" como placeholders para prevenir inyección SQL
@@ -138,7 +150,7 @@ app.put('/menu/:id', async (req, res) => {
         const [result] = await db.query(query, [nombre_plato, descripcion, precio, categoria, calorias, disponible, id]);
         res.status(201).json({
             mensaje: 'Plato actualizado con éxito',
-            id: result.insertId // Retornamos el ID generado en la DB
+            id: result.insertId
         });
     } catch (error) {
         res.status(500).json({ error: 'Error al guardar en la base de datos' });
